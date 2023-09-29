6bed7fdeb5142570ddc94ce676fad8663c13454e
Joe Asercion <Joseph.A.Asercion@nasa.gov>
2023-09-29 15:06:57 -0400

    add GuildMembers intent

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client, Collection, Events, GatewayIntentBits } = require( 'discord.js')
 const { token } = require('./config.json');
 
 // Create new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
