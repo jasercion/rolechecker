18cd8950b78b27e86bb25fec07d807b77a19f7e1
jasercion <jasercion.gwu@gmail.com>
2023-10-01 23:16:39 -0400

    add comments

--- commands/lookup/checkrole.js
+++ commands/lookup/checkrole.js
@@ -1,3 +1,7 @@
+/* 
+File detailing the checkrole query command.  
+*/
+
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
 
@@ -12,7 +16,7 @@ module.exports = {
     async execute(interaction) {
         const searchrole = interaction.options.getRole('role');
         const guild = interaction.guild;
-        await guild.members.fetch();
+        await guild.members.fetch(); /* This is required to refresh the bot cache with ALL server members */
         const uids = searchrole.members.map(m => m.id).join('\n');
         const filename = `${guild.name}_${searchrole.name}UIDs.csv`;
         const filepath = `/tmp/${filename}`;
@@ -21,6 +25,7 @@ module.exports = {
                 console.error(err);
             }
         });
+        /* Return message format: */
         await interaction.reply({ content: `UIDs with ${searchrole.name}:`, ephemeral: true, files: [{attachment: filepath, name: filename}]});
     },
 };
